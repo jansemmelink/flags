dbb2f34aea3fac782b716fe5ec52430bab975a09
Jan Semmelink <jansemmelink@conor.co.za>
2018-09-06 12:05:49 +0200

    Updated to show how to sub-select a set of flags... still very verbose, but good to show

--- example2/main.go
+++ example2/main.go
@@ -5,12 +5,14 @@
 //
 // Example:
 //----------------------------------------------------------------------------
-//	./example2 -g -d true -h --logfile=/tmp/s -o del
-//	2018/09/06 11:54:07 DEBUG Mode
-//	2018/09/06 11:54:07 Logfile = /tmp/s
-//	2018/09/06 11:54:07 Remaining arguments: [-g -h -o del]
-//	2018/09/06 11:54:07 Operation="del"
-//	2018/09/06 11:54:07 Remaining arguments: [-g -h]
+//	./example2 -g -d true -h --logfile=/tmp/s -o upd -n Joe -v Sam
+//	2018/09/06 12:04:51 DEBUG Mode
+//	2018/09/06 12:04:51 Logfile = /tmp/s
+//	2018/09/06 12:04:51 Remaining arguments: [-g -h -o upd -n Joe -v Sam]
+//	2018/09/06 12:04:51 Operation="upd"
+//	2018/09/06 12:04:51 Remaining arguments: [-g -h -n Joe -v Sam]
+//	2018/09/06 12:04:51 DOING oper=upd with flag{-n:Joe}flag{-v:Sam}
+//	2018/09/06 12:04:51 Remaining arguments: [-g -h]
 //----------------------------------------------------------------------------
 package main
 
@@ -64,9 +66,27 @@ func main() {
 	//now say wait to determine the operation from -o <oper> or --oper=<oper>
 	//then continue parsing operation specified options, we can do this:
 
-	opers := []string{"add", "get", "upd", "del"}
+	operSpecificFlags := make(map[string]*flags.Set)
+
+	operSpecificFlags["add"] = flags.NewSet("add", "Add")
+	operSpecificFlags["add"].String("-n", "--name", "", "Name to add")
+
+	operSpecificFlags["get"] = flags.NewSet("get", "Get")
+
+	operSpecificFlags["upd"] = flags.NewSet("upd", "Update")
+	operSpecificFlags["upd"].String("-n", "--name", "", "Name to update")
+	operSpecificFlags["upd"].String("-v", "--value", "", "New Value to use")
+
+	operSpecificFlags["del"] = flags.NewSet("del", "Delete")
+	operSpecificFlags["del"].String("-n", "--name", "", "Name to delete")
+
+	operNames := []string{}
+	for n := range operSpecificFlags {
+		operNames = append(operNames, n)
+	}
+
 	operSelectorSet := flags.NewSet("operation", "Operation")
-	operSelectorSet.String("-o", "--oper", "", fmt.Sprintf("Select operation to do %v", opers))
+	operSelectorSet.String("-o", "--oper", "", fmt.Sprintf("Select operation to do %v", operNames))
 
 	args, err = operSelectorSet.ParseKnown(args)
 	if err != nil {
@@ -74,9 +94,8 @@ func main() {
 		panic(fmt.Sprintf("Failed to parse operation flags: %v", err))
 	}
 
-	log.Printf("Operation=\"%s\"", operSelectorSet.Flag("-o").Value().(string))
-
-	//NOTE: The usage above and here only shows that set of operation, not the complete set... solved in next examples.
+	operName := operSelectorSet.Flag("-o").Value().(string)
+	log.Printf("Operation=\"%s\"", operName)
 
 	//show what args remained
 	if len(args) > 0 {
@@ -84,4 +103,26 @@ func main() {
 	} else {
 		log.Printf("No remaining arguments\n")
 	}
+
+	operSpecificSet, ok := operSpecificFlags[operName]
+	if !ok {
+		log.Printf("ERROR: Unknown operation \"%s\", expected %v", operName, operNames)
+	}
+
+	args, err = operSpecificSet.ParseKnown(args)
+	if err != nil {
+		operSpecificSet.PrintUsage(os.Stderr)
+		panic(fmt.Sprintf("Failed to parse operation specific flags: %v", err))
+	}
+
+	log.Printf("DOING oper=%s with %+v", operName, operSpecificSet)
+
+	//show what args remained
+	if len(args) > 0 {
+		log.Printf("Remaining arguments: %v\n", args)
+	} else {
+		log.Printf("No remaining arguments\n")
+	}
+
+	//NOTE: The usage in each case shows only that set, not the complete set... solved in next examples.
 } //main()
